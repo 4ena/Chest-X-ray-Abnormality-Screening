cb39d6854234217e320b188d2711e3d8a104cb49
4ena <242501219+4ena@users.noreply.github.com>
2026-03-28 19:18:28 -0400

    insert nn model

---
+++ main.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from config import DEVICE, NUM_EPOCHS, LEARNING_RATE
+from src.model import get_model
+from src.train import train_one_epoch
+from src.evaluate import evaluate
+from src.dataset import get_dataloaders
+
+def main():
+    train_loader, val_loader = get_dataloaders()
+
+    model = get_model().to(DEVICE)
+    criterion = nn.BCEWithLogitsLoss()
+    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+    for epoch in range(NUM_EPOCHS):
+        train_loss = train_one_epoch(model, train_loader, criterion, optimizer, DEVICE)
+        val_loss, val_acc = evaluate(model, val_loader, criterion, DEVICE)
+
+        print(f"Epoch {epoch + 1}/{NUM_EPOCHS}")
+        print(f"Train Loss: {train_loss:.4f}")
+        print(f"Val Loss: {val_loss:.4f} | Val Acc: {val_acc:.4f}")
+
+if __name__ == "__main__":
+    main()
+
+
+#def main():
+    #model = get_model().to(DEVICE)
+   # print("Model loaded successfully.")
+   # print("Using device:", DEVICE)
+
+   # dummy_input = torch.randn(4, 3, IMAGE_SIZE, IMAGE_SIZE).to(DEVICE)
+   # output = model(dummy_input)
+
+   # print("Output shape:", output.shape)
+
+#if __name__ == "__main__":
+    #main()
